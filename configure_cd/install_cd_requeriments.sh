ab4e0d3f444d7f70d6339aa898e5a2b01bf219b6
Ubuntu <ubuntu@ip-10-0-0-120.us-east-2.compute.internal>
2021-04-22 04:07:58 +0000

    adding script

---
+++ configure_cd/install_cd_requeriments.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "=========== INSTALLING =============="
+echo "python3"
+echo "pip3"
+echo "unzip"
+
+sudo apt-get install -y python3
+
+sudo apt-get install -y python3-pip unzip
+
+echo "boto3 using pip"
+echo "botocore using pip"
+echo "ansible using pip"
+
+sudo pip3 install boto3 botocore ansible
+
+echo "Exporting ansible"
+
+echo "export PATH=$PATH:$HOME/.local/bin" >> ~/.bashrc
+
+echo "Installing AWSCLI"
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+unzip awscliv2.zip
+
+sudo ./aws/install
+
+sudo mkdir /etc/ansible
+
+sudo curl https://raw.githubusercontent.com/ansible/ansible/devel/examples/ansible.cfg -o ansible.cfg
