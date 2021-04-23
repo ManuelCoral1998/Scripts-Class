d36a0329667756396b49afcce6469f43ec09c801
ManuelCoral1998 <43486285+ManuelCoral1998@users.noreply.github.com>
2021-04-22 19:41:05 -0500

    Update install_cd_requeriments.sh

--- configure_cd/install_cd_requeriments.sh
+++ configure_cd/install_cd_requeriments.sh
@@ -21,7 +21,7 @@ echo "export PATH=$PATH:$HOME/.local/bin" >> ~/.bashrc
 
 echo "Installing AWSCLI"
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 
 unzip awscliv2.zip
 
